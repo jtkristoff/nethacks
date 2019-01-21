a0d4ba56be213a60b5c8c473b69fdf76d64974ed
John Kristoff <jtkristoff@users.noreply.github.com>
2019-01-21 14:26:13 -0600

    new example tool, tc-asn.py

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Network tools and hacks
 * [random-ipv4addrs.py](random-ipv4addrs.py) rough, simpler equivalent of the Perl version
 * [randomize-lines.pl](randomize-lines.pl) randomize lines in a file
 * [pcapr.sh](pcapr.sh) capture traffic and write pcap files at regular intervals
+* [tc-asn.py](tc-asn.py) map IP addresses to origin ASN with Cymru's address mapping service
---
+++ tc-asn.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+
+import dns.resolver
+import dns.reversename
+import fileinput
+import re
+import sys
+
+def get_ptr_name(addr):
+    """
+    Return a Cymru BGP lookup PTR name given an IPv4 or IPv6 address
+    """
+
+    # use library to get reverse name
+    name = dns.reversename.from_address(addr).to_text(omit_final_dot=True)
+    # remove in-addr.arpa or ip6.arpa suffix
+    name = name.rsplit('.', 2)[0]
+
+    # append v4 or v6 suffix depending on ip version
+    p = re.compile(':')    # an ipv6 addr has at least one colon
+    m = p.search(addr)
+    if m:
+        name += '.origin6.asn.cymru.com.'
+    else:
+        name += '.origin.asn.cymru.com.'
+
+    return name
+
+def get_asn(addr):
+    """
+    Return a list of origin ASNs given an IPv4 or IPv6 address
+    """
+
+    qname = get_ptr_name(addr)
+
+    try:
+        answer = dns.resolver.query(qname, rdtype='TXT', rdclass='IN')
+    except:
+        # TODO: send this to stderr, log, or use traceback.format_exc()
+        return ['NA']
+
+    asns = []
+    for rdata in answer:
+        # https://www.cymru.com/jtk/blog/2010/08/10/#parsing-tcbgp-mapping
+        #   "49152 [...] | 192.0.2.0/24 | AA | registry | 1970-01-01"
+        #   [...]
+        # parse each TXT line, extract one or more ASNs per line
+        rdatatxt = rdata.strings
+        for line in rdatatxt:
+            asnstr = re.compile('\s*|\s*').split(line)[0]
+            asnlist = asnstr.split()
+            asns.extend(asnlist)
+
+    return asns
+
+# expect IP addrs, one per line
+for line in fileinput.input():
+    line = line.rstrip()
+
+    asns = get_asn(line)
+    for asn in asns:
+        sys.stdout.write('%-10s | %s\n' % (asn, line))
