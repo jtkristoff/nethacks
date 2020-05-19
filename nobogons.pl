259ee5c704e2b2190a2433ceec2af3f2405fc6af
John Kristoff <jtkristoff@users.noreply.github.com>
2020-05-19 17:48:06 -0500

    v6 support, verbose option, less input checking

--- nobogons.pl
+++ nobogons.pl
@@ -2,28 +2,29 @@
 use strict;
 use warnings;
 
+$| = 1;
+
 # WARNING: this code does not surpress less specific prefixes containing bogons (e.g. 0.0.0.0/0).
 
-use English;
-use Net::Patricia;
 use Getopt::Std;
+use Net::Patricia;
 
-$OUTPUT_AUTOFLUSH = 1;
+my $pt_bogon  = new Net::Patricia;
+my $pt_bogon6 = new Net::Patricia(AF_INET6);
 
-use constant SUCCESS => 0;
-use constant FAILURE => 1;
+# -f   IPv4_bogon_list, one prefix per line (optional, default=built-in)
+# -s   IPv6_bogon_list, one prefix per line (optional, default=built-in)
+# -v   enable_verbose (default=disabled)
 
-my $pt_bogon = new Net::Patricia;
-
-getopts( 'c:q', \ my %opts );
+getopts( 'f:s:v', \ my %opts );
 
 my @bogons;
-if ( $opts{c} ) {
+if ( $opts{f} ) {
     # try: http://www.team-cymru.org/Services/Bogons/fullbogons-ipv4.txt
-    parse_config( $opts{c} );
+    import_bogons( { file => $opts{f}, pt => $pt_bogon } );
 }
 else {
-    # current as of 2017-02-26
+    # current as of 2020-05-19
     @bogons = qw(
         0.0.0.0/8
         10.0.0.0/8
@@ -33,6 +34,8 @@ else {
         172.16.0.0/12
         192.0.0.0/24
         192.0.2.0/24
+        192.18.0.0/15
+        192.88.99.0/24
         192.168.0.0/16
         198.18.0.0/15
         198.51.100.0/24
@@ -44,98 +47,71 @@ else {
     }
 }
 
-# (-q) quick - no input verification
-# input is a per line, left-aligned, IPv4 dotted decimal w/ optional slash-notation mask
-if ( $opts{q} ) {
-    while ( defined(my $line=<>) ) {
-        chomp $line;
-
-        # bogon check
-        next if $pt_bogon->match_string($line);
-
-        # not a bogon, print it out
-        print "$line\n";
-    }
+my @bogons6;
+if ( $opts{s} ) {
+    # try: http://www.team-cymru.org/Services/Bogons/fullbogons-ipv6.txt
+    import_bogons( { file => $opts{s}, pt => $pt_bogon6 } );
 }
-# default with input validation, slower
 else {
-    while ( defined(my $line=<>) ) {
-        chomp $line;
-
-        # valid IPv4 dotted decimal check
-        rm_lead_trail_whitespace(\$line);
-        next if ! valid_v4prefix($line);
-
-        # bogon check
-        next if $pt_bogon->match_string($line);
-
-        # not a bogon, print it out
-        print "$line\n";
+    # current as of 2020-05-19
+    @bogons6 = qw(
+        ::/128
+        ::1/128
+        ::ffff:0:0/96
+        64:ff9b:1::/48
+        100::/64
+        2001::/32
+        2001:2::/48
+        2001:db8::/32
+        2002::/16
+    );
+    for (@bogons6) {
+        $pt_bogon6->add_string($_);
     }
 }
 
-# remove leading and trailing whitespace from a string
-sub rm_lead_trail_whitespace {
-    my $string_ref = shift || return;
-    $$string_ref =~ s{ \A \s* }{}xms;
-    $$string_ref =~ s{ \s* \Z }{}xms;
-    return;
-}
+while ( defined(my $line=<>) ) {
+    # remove white space
+    chomp $line;
+    $line =~ s{ \A \s* }{}xms;
+    $line =~ s{ \s* \z }{}xms;
 
-# IPv4 octet and address range check
-sub valid_v4addr {
-    my $addr = shift;
-
-    # weak IPv4 address format check
-    return FAILURE if $addr !~ m{ \A \d{1,3} (?: [.] \d{1,3} ){3} \Z }xms;
-
-    # each octet should be 0 to 255
-    my @bytes = split /\./, $addr;
-    for my $byte (@bytes) {
-        return FAILURE if $byte > 255;
+    # IPv6?
+    if ( $line =~ m{ [:] }xms ) {
+        if ( $pt_bogon6->match_string($line) ) { 
+            print STDERR "bogon6 detected, skipping: $line\n" if $opts{v};
+            next;
+        }
+    }
+    else {
+        if ( $pt_bogon->match_string($line) ) {        
+            print STDERR "bogon detected, skipping: $line\n" if $opts{v};
+            next;
+        }
     }
 
-    my $int = unpack( 'N', pack('C4', @bytes) );
-    return FAILURE if $int > 4294967295; # 255.255.255.255 = int 4294967295
-
-    return SUCCESS;
-}
-
-# IPv4 prefix format check
-sub valid_v4prefix {
-    my $prefix = shift;
-
-    # in the off chance the delimter is the trailing character
-    return FAILURE if $prefix =~ m{ [/] \Z }xms;
-
-    my ( $addr, $mask )  = split /\//, $prefix;
-
-    return FAILURE if ! valid_v4addr($addr);
-
-    # unspecified mask is OK, trated as a /32
-    return SUCCESS if !defined($mask);
-
-    return FAILURE if $mask !~ m{ \A \d{1,2} \Z }xms;
-
-    return FAILURE if $mask < 0 || $mask > 32;
-
-    return SUCCESS;
+    # not a bogon, print it out
+    print "$line\n";
 }
 
 # parse a file that should contain one IPv4 dotted decimail per line
-sub parse_config {
-    my $filename = shift;
+sub import_bogons {
+    my ($arg_ref) = @_;
+    my $file      = $arg_ref->{file} or return;
+    my $pt        = $arg_ref->{pt} or return;
 
-    open( my $CONFIG_FILE, '<', $filename)
-        or  die "Unable to open $filename: $!\n";
+    open( my $CONFIG_FILE, '<', $file )
+        or  die "Unable to open $file: $!\n";
 
     while (defined (my $line=<$CONFIG_FILE>) ) {
         chomp $line;
+        $line =~ s{ \A \s* }{}xms;
+        $line =~ s{ \s* \z }{}xms;
 
-        rm_lead_trail_whitespace(\$line);
-        next if ! valid_v4prefix($line);
-
-        # push CIDR block onto bogon array
-        $pt_bogon->add_string($line);
+        # skip blank lines or comments
+        next if $line =~ m{ \A \s* (?: [#] .* )? \z }xms;
+        
+        # push CIDR block onto pt array
+        $pt->add_string($line);
     }
 }
